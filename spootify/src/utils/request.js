82a8bd3b4b5e097aa876b13229c957a5d0876cc1
Mhel Pineda <mhelpineda14@gmail.com>
2021-03-06 14:55:14 +0800

    fix indentation

--- spootify/src/utils/request.js
+++ spootify/src/utils/request.js
@@ -3,34 +3,34 @@ import { readFromCache, writeToCache } from './cache';
 import api from '../config';
 
 const getRawData = async (config, path, cacheEnabled = false) => {
-	const { data } = await axios(config);
-	cacheEnabled && writeToCache(path, data);
-	return data;
+  const { data } = await axios(config);
+  cacheEnabled && writeToCache(path, data);
+  return data;
 };
 
-const getCachedData = (url) => readFromCache(url);
+const getCachedData = url => readFromCache(url);
 
-const fetcher = async (path = '', token = '') => {
-	const { api: { baseUrl } } = api;
-	try {
-		const data = await getRawData(
-			{
-				url: baseUrl + path,
-				method: 'get',
-				headers: {
-					'Authorization': `Bearer ${token}`
-				}
-			},
-			path,
-			'enableCache'
-		);
-
-		if (data) {
-			return data;
-		}
-	} catch (err) {
-		console.error(`${path} `, err);
-	}
-};
+const fetcher = async (path = '', token='') => {
+  const { api: { baseUrl } } = api
+  try {
+    const data = await getRawData(
+      {
+        url: baseUrl+path,
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      },
+      path,
+      'enableCache'
+    );
+    
+    if(data) {
+      return data
+    }
+  } catch(err) {
+    console.error(`${path} `, err)
+  }
+}
 
 export { getCachedData, getRawData, fetcher };
